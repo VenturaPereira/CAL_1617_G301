4fe8aa293d0777e76032c7e2dd3d395dcb0d7aa8
up201503447 <up201503447@fe.up.pt>
2017-03-31 12:53:38 +0100

    Tentativa de atualizar à força arrastando os ficheiros

--- FichaJUNG.cpp
+++ FichaJUNG.cpp
@@ -3,158 +3,99 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include "Graph.h"
+using namespace std;
 
-void exercicio1();
-void exercicio2();
-void exercicio3();
-void exercicio4();
+struct VertexInfo{
 
-void exercicio1()
-{
-	GraphViewer *gv = new GraphViewer(600, 600, true);
-
-	gv->setBackground("background.jpg");
-
-	gv->createWindow(600, 600);
-
-	gv->defineEdgeDashed(true);
-	gv->defineVertexColor("blue");
-	gv->defineEdgeColor("black");
-
-	gv->addNode(0);
-	gv->addNode(1);
-	gv->addEdge(0, 0, 1, EdgeType::UNDIRECTED);
-
-	Sleep(2000); // use sleep(1) in linux ; Sleep(2000) on Windows
-
-	gv->removeEdge(0);
-	gv->removeNode(1);
-	gv->addNode(2);
-
-	gv->rearrange();
-
-	Sleep(2000);
-
-	gv->addEdge(1, 0, 2, EdgeType::UNDIRECTED);
-	gv->defineEdgeCurved(false);
-
-	gv->setVertexLabel(0, "Isto e um no");
-	gv->setEdgeLabel(1, "Isto e uma aresta");
-
-	gv->setVertexColor(2, "green");
-	gv->setEdgeColor(1, "yellow");
-
-	gv->setVertexSize(2, 40);
-	gv->setVertexIcon(0, "icon.gif");
-
-	gv->setEdgeDashed(1, false);
-
-	gv->rearrange();
-
-}
-
-void exercicio2()
-{
-	GraphViewer *gv = new GraphViewer(600, 600, false);
-
-	gv->createWindow(600, 600);
-
-	gv->defineEdgeColor("blue");
-	gv->defineVertexColor("yellow");
-	gv->addNode(0,300,50);
-	gv->addNode(1,318,58);
-	gv->addNode(4,300,100);
-	gv->addNode(7,282,58);
-	gv->addNode(2,325,75);
-	gv->addNode(3,318,93);
-	gv->addNode(6,275,75);
-	gv->addNode(5,282,93);
-
-	gv->addNode(8,150,200);
-
-	gv->setVertexColor(8, "green");
-	gv->setVertexColor(9, "blue");
-	gv->setVertexColor(10, "green");
-	gv->setVertexColor(11, "blue");
-	gv->setVertexColor(12, "green");
-	gv->setVertexColor(13, "green");
-	gv->addNode(9,300,200);
-	gv->addNode(10,450,200);
-	gv->addNode(11,300,400);
-
-	gv->addNode(12,200,550);
-	gv->addNode(13,400,550);
-
-	gv->addEdge(0, 0, 1, EdgeType::UNDIRECTED);
-	gv->addEdge(1, 1, 2, EdgeType::UNDIRECTED);
-	gv->addEdge(2, 2, 3, EdgeType::UNDIRECTED);
-	gv->addEdge(3, 3, 4, EdgeType::UNDIRECTED);
-	gv->addEdge(4, 4, 5, EdgeType::UNDIRECTED);
-	gv->addEdge(5, 5, 6, EdgeType::UNDIRECTED);
-	gv->addEdge(6, 6, 7, EdgeType::UNDIRECTED);
-	gv->addEdge(7, 7, 0, EdgeType::UNDIRECTED);
-
-	gv->addEdge(8, 4, 9, EdgeType::UNDIRECTED);
-	gv->addEdge(9, 9, 8, EdgeType::UNDIRECTED);
-	gv->addEdge(10, 9, 10, EdgeType::UNDIRECTED);
-	gv->addEdge(11, 9, 11, EdgeType::UNDIRECTED);
-	gv->addEdge(12, 11, 12, EdgeType::UNDIRECTED);
-	gv->addEdge(13, 11, 13, EdgeType::UNDIRECTED);
+	int idNo, X,Y;
+	string label;
+};
 
 
-	gv->rearrange();
-	bool first=true;
+void printGraph();
 
-	while(1)
-	{
-		Sleep(2000);
-		if (first)
-		{
-		  gv->removeNode(12);
-		  gv->removeNode(13);
-		  first=false;
-		}
-		else
-		{
-		  gv->removeNode(20);
-		  gv->removeNode(21);
-		}
-		gv->addNode(14,250,550);
-		gv->addNode(15,350,550);
-		gv->addEdge(14, 11, 14, EdgeType::UNDIRECTED);
-		gv->addEdge(15, 11, 15, EdgeType::UNDIRECTED);
-		gv->rearrange();
+void initialise();
 
-		Sleep(2000);
-		gv->removeNode(14);
-		gv->removeNode(15);
-		gv->addNode(16,300,550);
-		gv->addNode(17,300,550);
-		gv->addEdge(16, 11, 16, EdgeType::UNDIRECTED);
-		gv->addEdge(17, 11, 17, EdgeType::UNDIRECTED);
-		gv->rearrange();
-		Sleep(2000);
+void initialise(){
 
-		gv->removeNode(16);
-		gv->removeNode(17);
-		gv->addNode(18,250,550);
-		gv->addNode(19,350,550);
-		gv->addEdge(18, 11, 18, EdgeType::UNDIRECTED);
-		gv->addEdge(19, 11, 19, EdgeType::UNDIRECTED);
-		gv->rearrange();
-		Sleep(2000);
+	Graph<VertexInfo>* g;
 
-		gv->removeNode(18);
-		gv->removeNode(19);
-		gv->addNode(20,200,550);
-		gv->addNode(21,400,550);
-		gv->addEdge(20, 11, 20, EdgeType::UNDIRECTED);
-		gv->addEdge(21, 11, 21, EdgeType::UNDIRECTED);
-		gv->rearrange();
+	ifstream inFile;
+
+	//Ler o ficheiro nos.txt
+	inFile.open("nos.txt");
+
+	if (!inFile) {
+		cerr << "Unable to open file nos.txt";
+		exit(1);   // call system to stop
 	}
+
+	string line;
+
+	//each node will be in this format: id;x;y;label
+	while(getline(inFile, line))
+	{
+		VertexInfo v;
+		stringstream linestream(line);
+		string data, label;
+		linestream >> v.idNo;
+
+		cout << v.idNo << endl;
+		getline(linestream, data, ';');  // read up-to the first ; (discard ;).
+		linestream >> v.X;
+
+		cout << v.X << endl;
+		getline(linestream, data, ';');  // read up-to the first ; (discard ;).
+		linestream >> v.Y;
+
+		cout << v.Y << endl;
+		getline(linestream, data, ';');  // read up-to the first ; (discard ;).
+		linestream >> v.label;
+		g->addVertex(v);
+	}
+
+	inFile.close();
+
+	//Ler o ficheiro arestas.txt
+
+	inFile.open("arestas.txt");
+
+	if (!inFile) {
+		cerr << "Unable to open file arestas.txt";
+		exit(1);   // call system to stop
+	}
+
+	vector<VertexInfo> vertices = g->dfs();
+	//each line in the format: idEdge;idSource;idDest;weight
+	while(getline(inFile, line))
+	{
+		stringstream linestream(line);
+		string data;
+		int idAresta=0;
+		int idNoOrigem=0;
+		int idNoDestino=0;
+		double weight = 0;
+
+		linestream >> idAresta;
+		getline(linestream, data, ';');  // read up-to the first ; (discard ;).
+		linestream >> idNoOrigem;
+		getline(linestream, data, ';');  // read up-to the first ; (discard ;).
+		linestream >> idNoDestino;
+		getline(linestream, data, ';');  // read up-to the first ; (discard ;).
+		linestream >> weight;
+		for (int i = 0; i < vertices.size(); i++)
+			if (vertices[i].idNo == idNoOrigem)
+				for (int j = 0; j < vertices.size(); j++)
+					if (j != i && vertices[j].idNo == idNoDestino)
+						g->addEdge(vertices[i], vertices[j], weight);
+	}
+
+	inFile.close();
+
 }
 
-void exercicio3()
+void printGraph()
 {
 	GraphViewer *gv = new GraphViewer(600, 600, false);
 
@@ -169,28 +110,32 @@ void exercicio3()
 	inFile.open("nos.txt");
 
 	if (!inFile) {
-	    cerr << "Unable to open file datafile.txt";
-	    exit(1);   // call system to stop
+		cerr << "Unable to open file nos.txt";
+		exit(1);   // call system to stop
 	}
 
-	std::string   line;
+	string line;
 
 	int idNo=0;
 	int X=0;
 	int Y=0;
+	string label;
 
-	while(std::getline(inFile, line))
+	while(getline(inFile, line))
 	{
-	    std::stringstream linestream(line);
-	    std::string         data;
+		stringstream linestream(line);
+		string data;
 
-	    linestream >> idNo;
+		linestream >> idNo;
 
-	    std::getline(linestream, data, ';');  // read up-to the first ; (discard ;).
-	    linestream >> X;
-	    std::getline(linestream, data, ';');  // read up-to the first ; (discard ;).
-	    linestream >> Y;
-	    gv->addNode(idNo,X,Y);
+		getline(linestream, data, ';');  // read up-to the first ; (discard ;).
+		linestream >> X;
+		getline(linestream, data, ';');  // read up-to the first ; (discard ;).
+		linestream >> Y;
+		getline(linestream, data, ';');  // read up-to the first ; (discard ;).
+		linestream >> label;
+		gv->addNode(idNo,X,Y);
+		gv->setVertexLabel(idNo, label);
 
 	}
 
@@ -200,40 +145,38 @@ void exercicio3()
 	//Ler o ficheiro arestas.txt
 	inFile.open("arestas.txt");
 
-		if (!inFile) {
-		    cerr << "Unable to open file datafile.txt";
-		    exit(1);   // call system to stop
-		}
+	if (!inFile) {
+		cerr << "Unable to open file arestas.txt";
+		exit(1);   // call system to stop
+	}
 
-		int idAresta=0;
-		int idNoOrigem=0;
-		int idNoDestino=0;
+	int idAresta=0;
+	int idNoOrigem=0;
+	int idNoDestino=0;
 
-		while(std::getline(inFile, line))
-		{
-		    std::stringstream linestream(line);
-		    std::string data;
+	while(getline(inFile, line))
+	{
+		stringstream linestream(line);
+		string data;
 
+		linestream >> idAresta;
 
-		    linestream >> idAresta;
+		getline(linestream, data, ';');  // read up-to the first ; (discard ;).
+		linestream >> idNoOrigem;
+		getline(linestream, data, ';');  // read up-to the first ; (discard ;).
+		linestream >> idNoDestino;
+		gv->addEdge(idAresta,idNoOrigem,idNoDestino, EdgeType::UNDIRECTED);
 
-		    std::getline(linestream, data, ';');  // read up-to the first ; (discard ;).
-		    linestream >> idNoOrigem;
-		    std::getline(linestream, data, ';');  // read up-to the first ; (discard ;).
-		    linestream >> idNoDestino;
-		    gv->addEdge(idAresta,idNoOrigem,idNoDestino, EdgeType::UNDIRECTED);
+	}
 
-		}
-
-		inFile.close();
+	inFile.close();
 
 	gv->rearrange();
 }
 
 int main() {
-	//exercicio1();
-	exercicio1();
-	//exercicio3();
+
+	printGraph();
 	getchar();
 	return 0;
 }
